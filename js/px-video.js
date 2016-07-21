97678e3e344b949d2c85bbf8f7f296710f860e63
Vidit Maniyar <vnmaniyar@paypal.com>
2016-07-21 03:03:41 -0700

    fixing position of the skip-to time element
    Fixed the skip-to time showing div position when the browser size was
    altered.

--- js/px-video.js
+++ js/px-video.js
@@ -501,7 +501,7 @@ function InitPxVideo(options) {
 
 		obj.progressBarHoverContainer.innerHTML = time;
 		obj.progressBarHoverContainer.style.position = "absolute";
-		obj.progressBarHoverContainer.style.left = (e.pageX - (this.offsetLeft / 2.5) )+"px";
+		obj.progressBarHoverContainer.style.left = (e.pageX - 25)+"px";
 		obj.progressBarHoverContainer.classList.remove("hide");
 	});
 
